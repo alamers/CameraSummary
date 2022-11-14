73d767d88109b7e2956de3235224465a7519f85f
Arjan Lamers <a.lamers@first8.nl>
2022-11-14 09:59:34 +0100

    added fatjar support added call to signal-cli

--- src/main/java/nl/aardbeitje/camerasummary/CameraSummary.java
+++ src/main/java/nl/aardbeitje/camerasummary/CameraSummary.java
@@ -1,11 +1,13 @@
 package nl.aardbeitje.camerasummary;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.time.Instant;
 import java.time.ZonedDateTime;
@@ -14,12 +16,20 @@ import java.util.List;
 import java.util.TimeZone;
 
 public class CameraSummary {
-    
 
     private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm");
+    private static String signalCliDir;
 
     public static void main(String[] args) throws IOException {
-        File dir = new File("/Volumes/cudacamz/images/");
+        
+        if(args.length!=2) {
+            System.out.println("Usage: java -jar camerasummary-0.0.1-SNAPSHOT.jar <images directory> <signal-cli-path>");
+            System.exit(-1);
+        }
+
+        String imagesDir = args[0];
+        signalCliDir = args[1];
+        File dir = new File(imagesDir);
         recurse(dir);
     }
 
@@ -45,10 +55,10 @@ public class CameraSummary {
         for (List<File> group : groups) {
             if (!group.isEmpty()) {
                 File firstFile = group.get(0);
-                
+
                 String filename = createFileName(firstFile);
                 File output = new File(firstFile.getParentFile().getAbsolutePath(), filename);
-                
+
                 System.out.println(output.getAbsolutePath());
                 List<FileInputStream> is = toInputStreamList(group);
 
@@ -59,14 +69,36 @@ public class CameraSummary {
                 for (FileInputStream fis : is) {
                     fis.close();
                 }
-                
-                for( File f : group) {
+
+                for (File f : group) {
                     f.delete();
                 }
-                
+
                 output.setLastModified(firstFile.lastModified());
-                
+                notify(output);
+
             }
+        }
+
+    }
+
+    private static void notify(File output) {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command(signalCliDir, "-u", "+31623822405", "send", "-m", output.getName(), "+31623822405", "-a", output.getAbsolutePath().toString());
+
+        try {
+            Process process = processBuilder.start();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            process.waitFor();
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
     }
